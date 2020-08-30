8da8301a208dade40cd462bf4909264e4faaca8c
Pavel Klecansky <klecanskypavel@gmail.com>
2020-08-30 18:09:54 +0200

    Complete tempConversion Exercise

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,12 +1,18 @@
-const ftoc = function() {
+const ftoc = function (fahrenheit) {
+  //Přepočet fahrenheita na celsia
+  let calculation = ((fahrenheit - 32) * 5) / 9;
+  //Zaokrouhlení na jedno desetiné místo
+  return Math.round(calculation * 10) / 10;
+};
 
-}
-
-const ctof = function() {
-
-}
+const ctof = function (celsius) {
+  //Přepočet celsia na fahrenheita
+  let calculation = celsius * 1.8 + 32;
+  //Zaokrouhlení na jedno desetiné místo
+  return Math.round(calculation * 10) / 10;
+};
 
 module.exports = {
   ftoc,
-  ctof
-}
+  ctof,
+};
