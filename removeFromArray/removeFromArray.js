286c9c262a195a4d3f049d902de388b2f9dd8459
Pavel Klecansky <klecanskypavel@gmail.com>
2020-08-30 16:48:59 +0200

    Complete removeFromArray Exercise

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...remove) {
+  for (let index = 0; index < remove.length; index++) {
+    const element = remove[index];
+    for (let arrayIndex = 0; arrayIndex < array.length; arrayIndex++) {
+      const indexOfRemove = array.indexOf(element);
+      if (indexOfRemove === -1) {
+          continue;
+      }
+      array.splice(indexOfRemove, 1);
+    }
+  }
 
-}
+  return array;
+};
 
-module.exports = removeFromArray
+module.exports = removeFromArray;
