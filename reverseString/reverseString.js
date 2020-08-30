e8d111dd9617916c4e7b4d3dba2f2113b763f95a
Pavel Klecansky <klecanskypavel@gmail.com>
2020-08-30 14:05:23 +0200

    Complete reverseString Exercise

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let reverse = "";
+    for (let index = string.length-1; index >= 0; index--) {
+        reverse += string.charAt(index);
+    }
+    return reverse;
 }
+
 
 module.exports = reverseString
