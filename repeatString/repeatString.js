a61a21a196dedfe1c304e426bfa9f3710d72c96e
Pavel Klecansky <klecanskypavel@gmail.com>
2020-08-30 13:43:27 +0200

    Complete repeatString Exercise

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string,numberOfTimes) {
+    if (numberOfTimes < 0) {
+        return "ERROR";
+    }
+    let newString = "";
+    for (let index = 0; index < numberOfTimes; index++) {
+        newString += string;
+    }
+    return newString;
 }
 
 module.exports = repeatString
