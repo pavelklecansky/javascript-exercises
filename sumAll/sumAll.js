1154b7a1d1831a396f68e5f48c649ebfde6036d0
Pavel Klecansky <klecanskypavel@gmail.com>
2020-08-30 17:50:00 +0200

    Complete sumAll Exercise

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function (startNumber, endNumber) {
+  if (
+    (typeof startNumber != "number" || typeof endNumber != "number") ||
+    (startNumber < 0 || endNumber < 0)
+  ) {
+    return "ERROR";
+  }
+  let sum = 0;
+  let biggerNumber = startNumber > endNumber ? startNumber : endNumber;
+  let smallerNumber = startNumber < endNumber ? startNumber : endNumber;
 
-}
+  for (let index = smallerNumber; index <= biggerNumber; index++) {
+    sum += index;
+  }
+  return sum;
+};
 
-module.exports = sumAll
+module.exports = sumAll;
