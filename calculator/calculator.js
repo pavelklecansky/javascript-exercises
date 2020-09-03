82f3e7ff7d3dd9090185f7b3b91898f3f74d8a4f
Pavel Klecansky <klecanskypavel@gmail.com>
2020-09-03 23:50:28 +0200

    Complete calculator

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,32 +1,42 @@
-function add () {
-	
+function add(num1, num2) {
+  return num1 + num2;
 }
 
-function subtract () {
-	
+function subtract(num1, num2) {
+  return num1 - num2;
 }
 
-function sum () {
-	
+function sum(array) {
+  let sum = 0;
+  array.forEach((element) => {
+    sum += element;
+  });
+  return sum;
 }
 
-function multiply () {
-	
+function multiply(array) {
+  return array.reduce((total, current) => (total *= current), 1);
 }
 
-function power() {
-	
+function power(num1, num2) {
+  return num1 ** num2;
 }
 
-function factorial() {
-	
+function factorial(num) {
+  if (num < 0) {
+    return -1;
+  } else if (num === 0) {
+    return 1;
+  } else {
+    return num * factorial(num - 1);
+  }
 }
 
 module.exports = {
-	add,
-	subtract,
-	sum,
-	multiply,
-    power,
-	factorial
-}
+  add,
+  subtract,
+  sum,
+  multiply,
+  power,
+  factorial,
+};
