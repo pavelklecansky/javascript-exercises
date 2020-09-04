c70ff4d3bc217929b180e4870be1310740084a0b
Pavel Klecansky <klecanskypavel@gmail.com>
2020-09-04 17:33:12 +0200

    Complete fibonacci

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+  if (num < 0) {
+    return "OOPS";
+  }
+  num--;
+  let a = 1,
+    b = 0,
+    temp;
 
-}
+  while (num >= 0) {
+    temp = a;
+    a = a + b;
+    b = temp;
+    num--;
+  }
 
-module.exports = fibonacci
+  return b;
+};
+
+console.log(fibonacci(2));
+
+module.exports = fibonacci;
