4a3f38ce24dc1257e4d8caeca4a2c1c764179654
Pavel Klecansky <klecanskypavel@gmail.com>
2020-09-04 11:41:03 +0200

    Complete palindromes

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function() {
+const palindromes = function (sentance) {
+  // Odstranění tečku vykřičník a otazník a převedené na malá písmena
+  const adjustedSentence = sentance.trim().replace(/[ !?.,]/g, "").toLowerCase();
+  const reverseSentence = adjustedSentence.split("").reverse().join("");
+  return adjustedSentence === reverseSentence;
+};
 
-}
-
-module.exports = palindromes
+module.exports = palindromes;
